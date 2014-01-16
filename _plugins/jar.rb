a57f84051b31af4f264dddffa48fe1b5d931afd5
aterai <at.terai@gmail.com>
2014-01-16 16:19:30 +0900

    2014-01-16

--- _plugins/jar.rb
+++ _plugins/jar.rb
@@ -11,22 +11,7 @@ class Jar < Liquid::Tag
     id   = [cat, cam].join("/")
     href = "example.jar"
     gaq  = %Q|_gaq.push(['_trackEvent', 'Jar', 'Download', '#{id}']);location.href='#{href}'|
-
-<<"EOS"
-<div class="ad_box">
-<!-- xrea, big, 336x280, 09/11/04 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:336px;height:280px"
-     data-ad-client="ca-pub-6939179021013694"
-     data-ad-slot="9248548235"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-<a href="#{href}" onclick="#{gaq}">Jar file(example.jar)</a>
-EOS
-
-    #%Q|<a href="#{href}" onclick="#{gaq}">Jar file(example.jar)</a>|
+    %Q|<a href="#{href}" onclick="#{gaq}">Jar file(example.jar)</a>|
   end
 
   Liquid::Template.register_tag "jar", self
